49dfccf794790a10ccfc499c8d159306df85e5da
KimDoHyun <kdhqwe1030@naver.com>
2024-12-08 21:58:17 +0900

    feat: home화면 구현

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import styled from 'styled-components';
+import Home from '../assets/Home.png';
+import windowEtc from '../assets/windowEtc.png';
 
 const Select = () => {
-  return <div></div>;
+  return (
+    <BaseContainer>
+      <HomeIcon src={Home} />
+      <MainSelectWrapper></MainSelectWrapper>
+      <WindowIcon src={windowEtc} />
+    </BaseContainer>
+  );
 };
 
 export default Select;
+
+const BaseContainer = styled.div`
+  width: 100%;
+  height: 44px;
+  border-bottom: 1px solid #c7c9cf;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px 20px;
+`;
+const MainSelectWrapper = styled.div`
+  flex-grow: 1;
+`;
+const HomeIcon = styled.img`
+  height: 16px;
+`;
+const WindowIcon = styled.img`
+  width: 120px;
+`;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,19 @@ import React from 'react';
 import styled from 'styled-components';
 import Header from '../components/Header';
 import List from '../components/List';
+import Select from '../components/Select';
+import MainScreen from '../assets/MainScreen.png';
+
 const Home = () => {
   return (
     <BaseContainer>
       <Header></Header>
       <ContentWrapper>
         <List></List>
+        <ListExceptContainer>
+          <Select></Select>
+          <Content $MainScreen={MainScreen}></Content>
+        </ListExceptContainer>
       </ContentWrapper>
     </BaseContainer>
   );
@@ -23,4 +30,20 @@ const ContentWrapper = styled.div`
   display: flex;
   height: calc(100vh - 66px);
   width: 100%;
+`;
+
+const Content = styled.div`
+  background-image: url(${({ $MainScreen }) => $MainScreen});
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  height: 100%;
+  width: 100%;
+`;
+
+const ListExceptContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
 `;
